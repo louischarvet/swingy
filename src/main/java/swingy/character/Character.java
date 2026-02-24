d83c7ba9a2e8707852d2824ab492a1c4a46ba74d
louischarvet <louis.charvet@proton.me>
2026-02-24 13:52:24 +0100

    minor changes in Character: attributes names

--- src/main/java/swingy/character/Character.java
+++ src/main/java/swingy/character/Character.java
@@ -10,58 +10,62 @@ import swingy.artifact.Armor;
 
 public abstract class Character {
 	@NotNull
-	@Size(min = 2)
-	private final String	_name;
-	private final String	_class; // Hero only ?
+	@Size(min = 1, max = 10, message = "Name must be 1 to 10 characters long.")
+	private final String	name;
 
-	private int	_level;
+//	@NotNull
+//	@Pattern()
+	private final String	klass; // Hero only ?
 
-	private int	_attack;
-	private int	_defense;
-	private int	_hitPoints;
+	private int	level;
 
-	protected Weapon	_weapon;
-	protected Armor	_armor;
-	protected Helm	_helm;
+	private int	attack;
+	private int	defense;
+	private int	hitPoints;
+	// hero & villain: attribut private int damagePoints ?
+
+	protected Weapon	weapon;
+	protected Armor	armor;
+	protected Helm	helm;
 
 	protected Character(Builder builder) {
-		this._name = builder._name;
-		this._class = builder._class;
+		this.name = builder.name;
+		this.klass = builder.klass;
 
-		this._level = builder._level;
-		this._attack = builder._attack;
-		this._defense = builder._defense;
-		this._hitPoints = builder._hitPoints;
+		this.level = builder.level;
+		this.attack = builder.attack;
+		this.defense = builder.defense;
+		this.hitPoints = builder.hitPoints;
 
-		this._weapon = null;
-		this._armor = null;
-		this._helm = null;
+		this.weapon = null;
+		this.armor = null;
+		this.helm = null;
 	}
 
 	public String	getName() {
-		return this._name;
+		return this.name;
 	}
 
-	public String	getCharacterClass() {
-		return this._class;
+	public String	getKlass() {
+		return this.klass;
 	}
 
 	public int	attackOutput() {
-		int	bonus = this._weapon != null ? this._weapon.getLevel() : 0;
+		int	bonus = this.weapon != null ? this.weapon.getLevel() : 0;
 		
 		return this._attack + bonus;
 	}
 
 	public int	defenseOutput() {
-		int	bonus = this._armor != null ? this._armor.getLevel() : 0;
+		int	bonus = this.armor != null ? this.armor.getLevel() : 0;
 		
 		return this._defense + bonus;
 	}
 
 	public int	hitPointsOutput() {
-		int	bonus = this._helm != null ? this._helm.getLevel() : 0;
+		int	bonus = this.helm != null ? this.helm.getLevel() : 0;
 		
-		return this._hitPoints + bonus;
+		return this.hitPoints + bonus;
 	}
 
 	public void	equip(Artifact artifact) {
@@ -75,13 +79,13 @@ public abstract class Character {
 
 		switch (artifactType) {
 			case "Weapon":
-				this._weapon = (Weapon)artifact;
+				this.weapon = (Weapon)artifact;
 				break;
 			case "Armor":
-				this._armor = (Armor)artifact;
+				this.armor = (Armor)artifact;
 				break;
 			case "Helm":
-				this._helm = (Helm)artifact;
+				this.helm = (Helm)artifact;
 				break;
 			default:
 				break;
@@ -89,47 +93,47 @@ public abstract class Character {
 	}
 
 	public void	print() { // abstract
-		String	attackBonus = this._weapon != null ?
-			(" \u001B[31m+" + this._weapon.getLevel()) + "\u001B[0m" : "";
-		String	defenseBonus = this._armor != null ?
-			(" \u001B[34m+" + this._armor.getLevel()) + "\u001B[0m" : "";
-		String	hitPointsBonus = this._helm != null ?
-			(" \u001B[32m+" + this._helm.getLevel()) + "\u001B[0m" : "";
+		String	attackBonus = this.weapon != null ?
+			(" \u001B[31m+" + this.weapon.getLevel()) + "\u001B[0m" : "";
+		String	defenseBonus = this.armor != null ?
+			(" \u001B[34m+" + this.armor.getLevel()) + "\u001B[0m" : "";
+		String	hitPointsBonus = this.helm != null ?
+			(" \u001B[32m+" + this.helm.getLevel()) + "\u001B[0m" : "";
 
 		System.out.print(
 			this.getName() + " | " + this.getCharacterClass()
-			+ " | LVL " + this._level
-			+ " | ATT " + this._attack + attackBonus
-			+ " | DEF " + this._defense + defenseBonus
-			+ " | HPS " + this._hitPoints + hitPointsBonus
+			+ " | LVL " + this.level
+			+ " | ATT " + this.attack + attackBonus
+			+ " | DEF " + this.defense + defenseBonus
+			+ " | HPS " + this.hitPoints + hitPointsBonus
 		);
 	}
 
 	public static abstract class Builder {
-		private String	_name;
-		private String	_class;
+		private String	name;
+		private String	klass;
 
-		private int	_level = 1;
-		private int	_attack = 1;
-		private int	_defense = 1;
-		private int	_hitPoints = 1;
+		private int	level = 1;
+		private int	attack = 1;
+		private int	defense = 1;
+		private int	hitPoints = 1;
 
-//		private Weapon	_weapon = null;
-//		private Armor	_armor = null;
-//		private Helm	_helm = null;
+//		private Weapon	weapon = null;
+//		private Armor	armor = null;
+//		private Helm	helm = null;
 
 		public Builder	setName(String p_name) {
-			this._name = p_name;
+			this.name = p_name;
 			return this;
 		}
 
 		public Builder	setClass(String p_class) {
-			this._class = p_class;
+			this.klass = p_class;
 			return this;
 		}
 
 		public Builder	setLevel(int p_level) {
-			this._level = p_level;
+			this.level = p_level;
 			return this;
 		}
 
