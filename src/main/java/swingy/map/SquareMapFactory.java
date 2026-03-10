d3db9b4dfdf9fca9a325c434fb69768825afd54d
louischarvet <louis.charvet@proton.me>
2026-03-10 16:04:06 +0100

    SquareMapFactory ok

--- src/main/java/swingy/map/SquareMapFactory.java
+++ src/main/java/swingy/map/SquareMapFactory.java
@@ -3,13 +3,6 @@ package swingy.map;
 import java.lang.Integer;
 
 public final class SquareMapFactory {
-	public static void	main(String args[]) {
-		if (args.length != 1)
-			return ;
-		SquareMap	grid = newSquareMap(Integer.parseInt(args[0]));
-		grid.print();
-	}
-
 	private	SquareMapFactory() {
 		super();
 	}
